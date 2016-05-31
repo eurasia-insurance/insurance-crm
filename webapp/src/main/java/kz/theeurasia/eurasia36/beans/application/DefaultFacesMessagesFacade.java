0b066efbd9aa5572b5fd13e73d4a86023f4a2109
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-05-31 16:16:11 +0600

    improvements

--- webapp/src/main/java/kz/theeurasia/eurasia36/beans/application/DefaultFacesMessagesFacade.java
+++ webapp/src/main/java/kz/theeurasia/eurasia36/beans/application/DefaultFacesMessagesFacade.java
@@ -6,6 +6,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import javax.inject.Inject;
 
 import kz.theeurasia.eurasia36.application.UIMessages;
@@ -20,21 +21,78 @@ public class DefaultFacesMessagesFacade implements FacesMessagesFacade {
 
     @Override
     public FacesMessage addMessage(UIMessages message) {
-	return addMessage(message, null, FacesMessage.SEVERITY_WARN, null);
+	FacesMessage fm = createMessage(message);
+	FacesContext.getCurrentInstance().addMessage(null, fm);
+	return fm;
     }
 
     @Override
     public FacesMessage addMessage(UIMessages message, FacesMessage.Severity severity) {
-	return addMessage(message, null, severity, null);
+	FacesMessage fm = createMessage(message, severity);
+	FacesContext.getCurrentInstance().addMessage(null, fm);
+	return fm;
     }
 
     @Override
     public FacesMessage addMessage(UIMessages message, Severity severity, String clientId) {
-	return addMessage(message, null, severity, clientId);
+	FacesMessage fm = createMessage(message, severity, clientId);
+	FacesContext.getCurrentInstance().addMessage(clientId, fm);
+	return fm;
     }
 
     @Override
-    public FacesMessage addMessage(UIMessages messageKey, UIMessages detailsKey, FacesMessage.Severity severity,
+    public FacesMessage addMessage(UIMessages message, UIMessages details, FacesMessage.Severity severity,
+	    String clientId) {
+	FacesMessage fm = createMessage(message, details, severity, clientId);
+	FacesContext.getCurrentInstance().addMessage(clientId, fm);
+	return fm;
+    }
+
+    @Override
+    public FacesMessage createMessage(UIMessages message) {
+	return generateMessage(message, null, FacesMessage.SEVERITY_WARN, null);
+    }
+
+    @Override
+    public FacesMessage createMessage(UIMessages message, Severity severity) {
+	return generateMessage(message, null, severity, null);
+    }
+
+    @Override
+    public FacesMessage createMessage(UIMessages message, Severity severity, String clientId) {
+	return generateMessage(message, null, severity, clientId);
+    }
+
+    @Override
+    public FacesMessage createMessage(UIMessages message, UIMessages details, Severity severity,
+	    String clientId) {
+	return generateMessage(message, details, severity, clientId);
+    }
+
+    @Override
+    public ValidatorException throwValidationException(UIMessages message) {
+	return new ValidatorException(createMessage(message, FacesMessage.SEVERITY_ERROR));
+    }
+
+    @Override
+    public ValidatorException throwValidationException(UIMessages message, Severity severity) {
+	return new ValidatorException(createMessage(message, severity));
+    }
+
+    @Override
+    public ValidatorException throwValidationException(UIMessages message, Severity severity, String clientId) {
+	return new ValidatorException(createMessage(message, severity, clientId));
+    }
+
+    @Override
+    public ValidatorException throwValidationException(UIMessages message, UIMessages details, Severity severity,
+	    String clientId) {
+	return new ValidatorException(createMessage(message, details, severity, clientId));
+    }
+
+    // PRIVATE
+
+    private FacesMessage generateMessage(UIMessages messageKey, UIMessages detailsKey, FacesMessage.Severity severity,
 	    String clientId) {
 	ResourceBundle ui = resourceBundleProducer.getUIMessages();
 	String message = null;
@@ -44,7 +102,6 @@ public class DefaultFacesMessagesFacade implements FacesMessagesFacade {
 	if (detailsKey != null)
 	    details = ui.getString(detailsKey.getKey());
 	FacesMessage facesMessage = new FacesMessage(severity, message, details);
-	FacesContext.getCurrentInstance().addMessage(null, facesMessage);
 	return facesMessage;
     }
 

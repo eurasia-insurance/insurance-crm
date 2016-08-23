fc7da38b7fc9dcc5a19e44c5e8df92036f7e5496
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-08-23 12:28:32 +0600

    zehr gut

--- webapp/src/main/java/kz/theeurasia/eurasia36/beans/view/InsuranceRequestHolder.java
+++ webapp/src/main/java/kz/theeurasia/eurasia36/beans/view/InsuranceRequestHolder.java
@@ -15,4 +15,23 @@ import kz.theeurasia.eurasia36.beans.api.WritableValueHolder;
 public class InsuranceRequestHolder extends DefaultWritableValueHolder<InsuranceRequest>
 	implements Serializable, WritableValueHolder<InsuranceRequest> {
     private static final long serialVersionUID = -2574434730269891652L;
+
+    public String getProductTypeVerb() {
+	if (value == null || value.getProductType() == null)
+	    return "none";
+	return value.getProductType().getVerb();
+    }
+
+    public String getObtainingMethodVerb() {
+	if (value == null || value.getObtaining() == null || value.getObtaining().getMethod() == null)
+	    return "none";
+	switch (value.getObtaining().getMethod()) {
+	case DELIVERY:
+	    return "delivery";
+	case PICKUP:
+	    return "pickup";
+	default:
+	    return "none";
+	}
+    }
 }

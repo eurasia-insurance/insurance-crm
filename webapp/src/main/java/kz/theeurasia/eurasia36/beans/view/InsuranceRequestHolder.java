703b57b1e2b3322dcfd9b084fd3be5da5704a465
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-08-23 17:03:36 +0600

    worked

--- webapp/src/main/java/kz/theeurasia/eurasia36/beans/view/InsuranceRequestHolder.java
+++ webapp/src/main/java/kz/theeurasia/eurasia36/beans/view/InsuranceRequestHolder.java
@@ -1,12 +1,16 @@
 package kz.theeurasia.eurasia36.beans.view;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.inject.Named;
 
 import org.omnifaces.cdi.ViewScoped;
 
 import com.lapsa.insurance.domain.InsuranceRequest;
+import com.lapsa.insurance.domain.casco.Casco;
+import com.lapsa.insurance.domain.casco.CascoVehicle;
 
 import kz.theeurasia.eurasia36.beans.api.WritableValueHolder;
 
@@ -42,5 +46,14 @@ public class InsuranceRequestHolder extends DefaultWritableValueHolder<Insurance
 	default:
 	    return VERB_NONE;
 	}
+    }
+
+    public List<CascoVehicle> getCascoVehiclesAsList() {
+	if (value != null && value.getProduct() != null && value.getProduct() instanceof Casco) {
+	    Casco casco = (Casco) value.getProduct();
+	    CascoVehicle vehicle = casco.getInsuredVehicle();
+	    return Arrays.asList(vehicle);
+	}
+	return null;
     }
 }

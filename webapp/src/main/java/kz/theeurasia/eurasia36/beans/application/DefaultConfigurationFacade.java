c1752e49880861e65d74064213fb5d89fea1030e
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-11-01 12:38:05 +0600

    #impl #6

--- webapp/src/main/java/kz/theeurasia/eurasia36/beans/application/DefaultConfigurationFacade.java
+++ webapp/src/main/java/kz/theeurasia/eurasia36/beans/application/DefaultConfigurationFacade.java
@@ -2,6 +2,10 @@ package kz.theeurasia.eurasia36.beans.application;
 
 import static kz.theeurasia.eurasia36.application.ParameterConstants.*;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.model.SelectItem;
 import javax.inject.Named;
@@ -35,5 +39,32 @@ public class DefaultConfigurationFacade implements ConfigurationFacade {
     @Override
     public SelectItem getMustSelectSI() {
 	return new SelectItem(null, "-- выберите значение --");
+    }
+
+    private static final String VERSION_RESOURCE_PATH = "/version.properties";
+    private static final String VERSION_PARAMETER_NAME = "version";
+    private static final String VERSION_DEFAULT = "UNKNOWN";
+
+    private String version;
+
+    @Override
+    public String getVersion() {
+	if (version == null)
+	    version = retreiveVersion();
+	return version;
+    }
+
+    // PRIVATE
+
+    private String retreiveVersion() {
+	try (InputStream stream = this.getClass().getResourceAsStream(VERSION_RESOURCE_PATH)) {
+	    if (stream == null)
+		return VERSION_DEFAULT;
+	    Properties props = new Properties();
+	    props.load(stream);
+	    return (String) props.get(VERSION_PARAMETER_NAME);
+	} catch (IOException e) {
+	    return VERSION_DEFAULT;
+	}
     }
 }

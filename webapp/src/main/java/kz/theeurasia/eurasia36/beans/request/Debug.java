8772a9fde1a12ed30cb4fee7fed927be8b2c8e6a
Vadim.Isaev <vadim.o.isaev@gmail.com>
2016-11-30 15:45:24 +0600

    функционал для тестирования исключений

--- webapp/src/main/java/kz/theeurasia/eurasia36/beans/request/Debug.java
+++ webapp/src/main/java/kz/theeurasia/eurasia36/beans/request/Debug.java
@@ -1,6 +1,8 @@
 package kz.theeurasia.eurasia36.beans.request;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.ViewExpiredException;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named
@@ -14,5 +16,21 @@ public class Debug {
 
     public String getRandomTag() {
 	return String.format("%1$03d", Math.round(Math.random() * 999));
+    }
+
+    public void throwException() {
+	throw new RuntimeException("Test web-page exception");
+    }
+
+    public void throwViewExpired() {
+	throw new ViewExpiredException("A ViewExpiredException!",
+		FacesContext.getCurrentInstance().getViewRoot().getViewId());
+    }
+
+    public void longAjaxRequest() {
+	try {
+	    Thread.sleep(10 * 1000);
+	} catch (InterruptedException ignored) {
+	}
     }
 }

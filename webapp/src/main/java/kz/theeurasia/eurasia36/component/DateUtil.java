267a5471a77ea9539814798a6544884f30f3577b
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-10 16:44:41 +0600

    aditional tags on CRM

--- webapp/src/main/java/kz/theeurasia/eurasia36/component/DateUtil.java
+++ webapp/src/main/java/kz/theeurasia/eurasia36/component/DateUtil.java
@@ -1,6 +1,7 @@
 package kz.theeurasia.eurasia36.component;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
@@ -15,12 +16,36 @@ public class DateUtil {
 	return checkIsToday(date);
     }
 
+    public boolean isTodayLocalDate(LocalDate localDate) {
+	return checkIsToday(localDate);
+    }
+
+    public boolean isTodayLocalDateTime(LocalDateTime localDateTime) {
+	return checkIsToday(localDateTime);
+    }
+
     public boolean isYesterday(Date date) {
 	return checkIsYesterday(date);
     }
 
+    public boolean isYesterdayLocalDate(LocalDate localDate) {
+	return checkIsYesterday(localDate);
+    }
+
+    public boolean isYesterdayLocalDateTime(LocalDateTime localDateTime) {
+	return checkIsYesterday(localDateTime);
+    }
+
     public boolean isTommorow(Date date) {
 	return checkIsTommorow(date);
+    }
+
+    public boolean isTommorowLocalDate(LocalDate localDate) {
+	return checkIsTommorow(localDate);
+    }
+
+    public boolean isTommorowLocalDateTime(LocalDateTime localDateTime) {
+	return checkIsTommorow(localDateTime);
     }
 
     // PUBLIC STATIC
@@ -31,14 +56,40 @@ public class DateUtil {
 	return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
-    public static boolean checkIsToday(Date date) {
-	if (date == null)
+    public static Date toDateLocalDate(LocalDate localDate) {
+	if (localDate == null)
+	    return null;
+	return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static Date toDateLocalDateTime(LocalDateTime localDateTime) {
+	if (localDateTime == null)
+	    return null;
+	return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    private static boolean checkIsToday(LocalDateTime localDateTime) {
+	if (localDateTime == null)
 	    return false;
-	LocalDate localDate = toLocalDate(date);
+	return checkIsToday(localDateTime.toLocalDate());
+    }
+
+    private static boolean checkIsToday(LocalDate localDate) {
+	if (localDate == null)
+	    return false;
 	LocalDate check = LocalDate.now();
 	return check.isEqual(localDate);
     }
 
+    @Deprecated
+    public static boolean checkIsToday(Date date) {
+	if (date == null)
+	    return false;
+	LocalDate localDate = toLocalDate(date);
+	return checkIsToday(localDate);
+    }
+
+    @Deprecated
     public static boolean checkIsYesterday(Date date) {
 	if (date == null)
 	    return false;
@@ -47,12 +98,39 @@ public class DateUtil {
 	return check.isEqual(localDate);
     }
 
+    public static boolean checkIsYesterday(LocalDate localDate) {
+	if (localDate == null)
+	    return false;
+	LocalDate check = LocalDate.now().minusDays(1);
+	return check.isEqual(localDate);
+    }
+
+    public static boolean checkIsYesterday(LocalDateTime localDateTime) {
+	if (localDateTime == null)
+	    return false;
+	return checkIsYesterday(localDateTime.toLocalDate());
+    }
+
+    @Deprecated
     public static boolean checkIsTommorow(Date date) {
 	if (date == null)
 	    return false;
 	LocalDate localDate = toLocalDate(date);
 	LocalDate check = LocalDate.now().plusDays(1);
 	return check.isEqual(localDate);
+    }
+
+    public static boolean checkIsTommorow(LocalDate localDate) {
+	if (localDate == null)
+	    return false;
+	LocalDate check = LocalDate.now().plusDays(1);
+	return check.isEqual(localDate);
+    }
+
+    public static boolean checkIsTommorow(LocalDateTime localDateTime) {
+	if (localDateTime == null)
+	    return false;
+	return checkIsTommorow(localDateTime.toLocalDate());
     }
 
 }

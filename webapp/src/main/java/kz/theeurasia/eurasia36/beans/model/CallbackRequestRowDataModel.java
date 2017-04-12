b465ac568df210d1713804708d11ff779103e5c3
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-04-12 11:50:10 +0600

    model payment method and status

--- webapp/src/main/java/kz/theeurasia/eurasia36/beans/model/CallbackRequestRowDataModel.java
+++ webapp/src/main/java/kz/theeurasia/eurasia36/beans/model/CallbackRequestRowDataModel.java
@@ -1,10 +1,12 @@
 package kz.theeurasia.eurasia36.beans.model;
 
 import com.lapsa.insurance.crm.InsuranceRequestType;
+import com.lapsa.insurance.crm.PaymentStatus;
 import com.lapsa.insurance.crm.TransactionProblem;
 import com.lapsa.insurance.crm.TransactionStatus;
 import com.lapsa.insurance.domain.CallbackRequest;
 import com.lapsa.insurance.elements.InsuranceProductType;
+import com.lapsa.insurance.elements.PaymentMethod;
 
 import kz.theeurasia.eurasia36.beans.api.RequestType;
 
@@ -43,5 +45,15 @@ public class CallbackRequestRowDataModel extends RequestRowDataModel<CallbackReq
     @Override
     public Double getAmount() {
 	return 0d;
+    }
+
+    @Override
+    public PaymentMethod getPaymentMethod() {
+	return PaymentMethod.UNDEFINED;
+    }
+
+    @Override
+    public PaymentStatus getPaymentStatus() {
+	return PaymentStatus.UNDEFINED;
     }
 }

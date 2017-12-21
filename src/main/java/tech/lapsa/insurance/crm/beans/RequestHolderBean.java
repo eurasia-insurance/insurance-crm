6d83774c0ada7b439f45b3c0d6ae111512b4843c
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-21 19:45:49 +0600

    request paid instance

--- src/main/java/tech/lapsa/insurance/crm/beans/RequestHolderBean.java
+++ src/main/java/tech/lapsa/insurance/crm/beans/RequestHolderBean.java
@@ -1,6 +1,7 @@
 package tech.lapsa.insurance.crm.beans;
 
 import java.io.Serializable;
+import java.time.Instant;
 
 import javax.inject.Named;
 
@@ -19,5 +20,44 @@ public class RequestHolderBean extends AWritableValueHolder<RequestRow<?>>
     @Override
     public void reset() {
 	this.value = null;
+    }
+
+    // paidAmount
+
+    private Double paidAmount;
+
+    @Override
+    public Double getPaidAmount() {
+	return paidAmount;
+    }
+
+    public void setPaidAmount(Double paidAmount) {
+	this.paidAmount = paidAmount;
+    }
+
+    // paidInstant
+
+    private Instant paidInstant;
+
+    @Override
+    public Instant getPaidInstant() {
+	return paidInstant;
+    }
+
+    public void setPaidInstant(Instant paidInstant) {
+	this.paidInstant = paidInstant;
+    }
+
+    // paidReference
+
+    private String paidReference;
+
+    @Override
+    public String getPaidReference() {
+	return paidReference;
+    }
+
+    public void setPaidReference(String paidReference) {
+	this.paidReference = paidReference;
     }
 }
--- src/main/java/tech/lapsa/insurance/crm/beans/i/RequestHolder.java
+++ src/main/java/tech/lapsa/insurance/crm/beans/i/RequestHolder.java
@@ -1,6 +1,15 @@
 package tech.lapsa.insurance.crm.beans.i;
 
+import java.time.Instant;
+
 import tech.lapsa.insurance.crm.rows.RequestRow;
 
 public interface RequestHolder extends WritableValueHolder<RequestRow<?>> {
+
+    // TODO REFACT :: Move to RequestRow
+    Double getPaidAmount();
+
+    Instant getPaidInstant();
+
+    String getPaidReference();
 }

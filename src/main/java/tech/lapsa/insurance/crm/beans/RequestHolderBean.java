6904c00b399778bdcfcc1beb84c6534501c146bf
Vadim.Isaev <vadim.o.isaev@gmail.com>
2017-12-22 18:10:17 +0600

    just a holder

--- src/main/java/tech/lapsa/insurance/crm/beans/RequestHolderBean.java
+++ src/main/java/tech/lapsa/insurance/crm/beans/RequestHolderBean.java
@@ -1,7 +1,6 @@
 package tech.lapsa.insurance.crm.beans;
 
 import java.io.Serializable;
-import java.time.Instant;
 
 import javax.inject.Named;
 
@@ -20,44 +19,5 @@ public class RequestHolderBean extends AWritableValueHolder<RequestRow<?>>
     @Override
     public void reset() {
 	this.value = null;
-    }
-
-    // paidAmount
-
-    private Double paidAmount;
-
-    @Override
-    public Double getPaidAmount() {
-	return paidAmount;
-    }
-
-    public void setPaidAmount(Double paidAmount) {
-	this.paidAmount = paidAmount;
-    }
-
-    // paidInstant
-
-    private Instant paidInstant;
-
-    @Override
-    public Instant getPaidInstant() {
-	return paidInstant;
-    }
-
-    public void setPaidInstant(Instant paidInstant) {
-	this.paidInstant = paidInstant;
-    }
-
-    // paidReference
-
-    private String paidReference;
-
-    @Override
-    public String getPaidReference() {
-	return paidReference;
-    }
-
-    public void setPaidReference(String paidReference) {
-	this.paidReference = paidReference;
     }
 }

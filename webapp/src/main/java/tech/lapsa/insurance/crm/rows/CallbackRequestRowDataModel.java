c79f29c101c452624c491f91051d57b986a4fec4
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-01-24 15:24:37 +0600

    callback request row

--- webapp/src/main/java/tech/lapsa/insurance/crm/rows/CallbackRequestRowDataModel.java
+++ webapp/src/main/java/tech/lapsa/insurance/crm/rows/CallbackRequestRowDataModel.java
@@ -1,22 +1,12 @@
 package tech.lapsa.insurance.crm.rows;
 
-import java.time.Instant;
-import java.util.Currency;
-
-import com.lapsa.insurance.domain.CalculationData;
 import com.lapsa.insurance.domain.CallbackRequest;
-import com.lapsa.insurance.domain.PaymentData;
 import com.lapsa.insurance.domain.casco.Casco;
 import com.lapsa.insurance.domain.policy.Policy;
-import com.lapsa.insurance.elements.InsuranceProductType;
-import com.lapsa.insurance.elements.InsuranceRequestType;
-import com.lapsa.insurance.elements.PaymentStatus;
-import com.lapsa.insurance.elements.TransactionProblem;
-import com.lapsa.insurance.elements.TransactionStatus;
 
 import tech.lapsa.insurance.crm.beans.i.RequestType;
 
-public class CallbackRequestRowDataModel extends RequestRowDataModel<CallbackRequest>
+public class CallbackRequestRowDataModel extends InsuranceRequestRowDataModel<CallbackRequest>
 	implements RequestRow<CallbackRequest> {
 
     public CallbackRequestRowDataModel(CallbackRequest entity) {
@@ -29,102 +19,12 @@ public class CallbackRequestRowDataModel extends RequestRowDataModel<CallbackReq
     }
 
     @Override
-    public TransactionStatus getTransactionStatus() {
-	return null;
-    }
-
-    @Override
-    public TransactionProblem getTransactionProblem() {
-	return null;
-    }
-
-    @Override
-    public InsuranceProductType getInsuranceProductType() {
-	return null;
-    }
-
-    @Override
-    public InsuranceRequestType getInsuranceRequestType() {
-	return null;
-    }
-
-    @Override
-    public Double getAmount() {
-	return null;
-    }
-
-    @Override
-    public Currency getCalculatedCurrency() {
-	return null;
-    }
-
-    @Override
-    public PaymentStatus getPaymentStatus() {
-	return PaymentStatus.UNDEFINED;
-    }
-
-    @Override
-    public String getPaymentInvoiceNumber() {
-	return null;
-    }
-
-    @Override
-    public String getPaymentReference() {
-	return null;
-    }
-
-    @Override
-    public String getAgreementNumber() {
-	return null;
-    }
-
-    @Override
-    public String getPaymentMethodName() {
-	return null;
-    }
-
-    @Override
-    public Instant getPaymentInstant() {
-	return null;
-    }
-
-    @Override
-    public PaymentData getPayment() {
-	return null;
-    }
-
-    @Override
-    public CalculationData getCalculation() {
-	return null;
-    }
-
-    @Override
     public Policy getPolicy() {
 	return null;
     }
 
     @Override
     public Casco getCasco() {
-	return null;
-    }
-
-    @Override
-    public Double getCalculatedAmount() {
-	return null;
-    }
-
-    @Override
-    public Double getPaymentAmount() {
-	return null;
-    }
-
-    @Override
-    public Currency getPaymentCurrency() {
-	return null;
-    }
-
-    @Override
-    public Currency getCurrency() {
 	return null;
     }
 }

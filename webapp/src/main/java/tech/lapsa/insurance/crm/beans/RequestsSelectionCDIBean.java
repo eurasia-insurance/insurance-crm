0acdb19ffa5b80e0a1b69d394866187668b7a5b3
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-04-25 09:44:22 +0600

    #cfrefact

--- webapp/src/main/java/tech/lapsa/insurance/crm/beans/RequestsSelectionCDIBean.java
+++ webapp/src/main/java/tech/lapsa/insurance/crm/beans/RequestsSelectionCDIBean.java
@@ -4,26 +4,68 @@ import java.io.Serializable;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.ejb.EJB;
 import javax.inject.Named;
 
 import org.omnifaces.cdi.ViewScoped;
 
-import tech.lapsa.insurance.crm.beans.i.RequestHolder;
+import com.lapsa.insurance.domain.Request;
+
 import tech.lapsa.insurance.crm.rows.RequestRow;
+import tech.lapsa.insurance.dao.RequestDAO.RequestDAORemote;
+import tech.lapsa.java.commons.exceptions.IllegalArgument;
+import tech.lapsa.java.commons.function.MyCollectors;
+import tech.lapsa.java.commons.function.MyObjects;
+import tech.lapsa.java.commons.function.MyStreams;
+import tech.lapsa.patterns.dao.NotFound;
 
 @Named("rqst")
 @ViewScoped
-public class RequestHolderBean extends AWritableValueHolder<List<RequestRow<?>>>
-	implements Serializable, RequestHolder {
+public class RequestsSelectionCDIBean implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Override
+    protected List<RequestRow<?>> value;
+
+    public List<RequestRow<?>> getValue() {
+	return value;
+    }
+
+    public void init() {
+	reset();
+    }
+
+    // EJBs
+
+    // insurance-dao (remote)
+
+    @EJB
+    private RequestDAORemote requestDAO;
+
+    private Request quickRefresh(Request r) {
+	try {
+	    return requestDAO.restore(r);
+	} catch (IllegalArgument e) {
+	    return null;
+	} catch (NotFound e) {
+	    return null;
+	}
+    }
+
+    public void setValue(List<RequestRow<?>> value) {
+	this.value = MyStreams.orEmptyOf(value) //
+		.map(RequestRow::getEntity) //
+		.map(this::quickRefresh) //
+		.filter(MyObjects::nonNull)
+		.map(RequestRow::from)
+		.collect(MyCollectors.unmodifiableList());
+    }
+
     public void reset() {
 	this.value = null;
     }
 
     public void setSingleRow(RequestRow<?> row) {
-	this.value = Arrays.asList(row);
+	setValue(Arrays.asList(row));
     }
 }

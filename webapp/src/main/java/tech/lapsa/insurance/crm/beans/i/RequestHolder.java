6eccda4f242df2fe04819e9a15c231bbbe8a4912
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-04-25 09:44:15 +0600

    remove interface for RequestsHolder

--- webapp/src/main/java/tech/lapsa/insurance/crm/beans/i/RequestHolder.java
+++
@@ -1,8 +0,0 @@
-package tech.lapsa.insurance.crm.beans.i;
-
-import java.util.List;
-
-import tech.lapsa.insurance.crm.rows.RequestRow;
-
-public interface RequestHolder extends WritableValueHolder<List<RequestRow<?>>> {
-}

1ef53c82a1eb7caa90233339960fe78644479bb5
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-04-30 11:18:03 +0600

    beans

--- webapp/src/main/java/tech/lapsa/insurance/crm/beans/UserListCDIBeans.java
+++ webapp/src/main/java/tech/lapsa/insurance/crm/beans/UserListCDIBeans.java
@@ -13,35 +13,98 @@ import com.lapsa.insurance.domain.crm.User;
 import tech.lapsa.insurance.facade.UserFacade.UserFacadeRemote;
 import tech.lapsa.java.commons.function.MyCollections;
 
-@Named("userList")
-@SessionScoped
-public class UserListCDIBean implements Serializable {
+public interface UserListCDIBeans extends Serializable {
 
-    private static final long serialVersionUID = 1L;
+    public static interface UserList {
 
-    private List<User> whoEverCreatedRequests;
-    private List<User> whoEverAcceptedRequests;
-    private List<User> whoEverCompletedRequests;
-
-    @PostConstruct
-    public void init() {
-	this.whoEverCreatedRequests = MyCollections.unmodifiableOrEmptyList(delegate.getWhoEverCreatedRequests());
-	this.whoEverAcceptedRequests = MyCollections.unmodifiableOrEmptyList(delegate.getWhoEverAcceptedRequests());
-	this.whoEverCompletedRequests = MyCollections.unmodifiableOrEmptyList(delegate.getWhoEverCompletedRequests());
+	List<User> getList();
     }
 
-    @EJB
-    private UserFacadeRemote delegate;
+    @Named("usersWhoEverCreatedRequests")
+    @SessionScoped
+    public static class UsersWhoEverCreatedRequestsCDIBean implements Serializable, UserList {
 
-    public List<User> getWhoEverCreatedRequests() {
-	return whoEverCreatedRequests;
+	private static final long serialVersionUID = 1L;
+
+	private List<User> list;
+
+	@PostConstruct
+	public void init() {
+	    this.list = MyCollections.unmodifiableOrEmptyList(delegate.getWhoEverCreatedRequests());
+	}
+
+	@EJB
+	private UserFacadeRemote delegate;
+
+	@Override
+	public List<User> getList() {
+	    return list;
+	}
     }
 
-    public List<User> getWhoEverAcceptedRequests() {
-	return whoEverAcceptedRequests;
+    @Named("usersWhoEverAcceptedRequests")
+    @SessionScoped
+    public static class UsersWhoEverAcceptedRequestsCDIBean implements Serializable, UserList {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<User> list;
+
+	@PostConstruct
+	public void init() {
+	    this.list = MyCollections.unmodifiableOrEmptyList(delegate.getWhoEverAcceptedRequests());
+	}
+
+	@EJB
+	private UserFacadeRemote delegate;
+
+	@Override
+	public List<User> getList() {
+	    return list;
+	}
     }
 
-    public List<User> getWhoEverCompletedRequests() {
-	return whoEverCompletedRequests;
+    @Named("usersWhoEverCompletedRequests")
+    @SessionScoped
+    public static class UsersWhoEverCompletedRequestsCDIBean implements Serializable, UserList {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<User> list;
+
+	@PostConstruct
+	public void init() {
+	    this.list = MyCollections.unmodifiableOrEmptyList(delegate.getWhoEverCompletedRequests());
+	}
+
+	@EJB
+	private UserFacadeRemote delegate;
+
+	@Override
+	public List<User> getList() {
+	    return list;
+	}
+    }
+
+    @Named("usersAll")
+    @SessionScoped
+    public static class UsersAllCDIBean implements Serializable, UserList {
+
+	private static final long serialVersionUID = 1L;
+
+	private List<User> list;
+
+	@PostConstruct
+	public void init() {
+	    this.list = MyCollections.unmodifiableOrEmptyList(delegate.getAll());
+	}
+
+	@EJB
+	private UserFacadeRemote delegate;
+
+	@Override
+	public List<User> getList() {
+	    return list;
+	}
     }
 }
